9805cf335abefe096e72d7e1e8b4db9869c0ce9c
mUniKeS <munikes@members.fsf.org>
2016-04-12 12:08:31 +0200

    Fix problem with string allowed host games.budismoclasico.org

--- wsgi/learn_tibetan/config/settings.py
+++ wsgi/learn_tibetan/config/settings.py
@@ -38,7 +38,7 @@ ALLOWED_HOSTS = [
     os.environ.get('OPENSHIFT_APP_DNS'), # Dynamically map to the OpenShift gear name.
     #'example.com', # First DNS alias (set up in the app)
     #'www.example.com', # Second DNS alias (set up in the app)
-    games.budismoclasico.org
+    'games.budismoclasico.org',
 ]
 
 
