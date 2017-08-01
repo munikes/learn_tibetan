d379e2779a2919f432355298737d6f06d9cff5c5
mUniKeS <munikes@members.fsf.org>
2017-08-01 20:11:14 +0200

    Add path to initial data and change static project files

--- wsgi/learntibetan/config/settings.py
+++ wsgi/learntibetan/config/settings.py
@@ -41,7 +41,7 @@ if ON_OPENSHIFT:
     SECRET_KEY = SECRETS['secret_key']
 else:
     # Make this unique, and don't share it with anybody.
-    SECRET_KEY = os.environ.get('SECRET_KEY')
+    SECRET_KEY = os.urandom(24).encode('hex')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG') == 'True'
@@ -104,8 +104,8 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/1.8/ref/settings/#databases
-
-DATABASES = {
+if ON_OPENSHIFT:
+    DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'learntibetan',          # Or path to database file if using sqlite3.
@@ -113,8 +113,16 @@ DATABASES = {
         'PASSWORD': DB_PASSWORD,   # Not used with sqlite3.
         'HOST': DB_HOST,       # Set to empty string for localhost. Not used with sqlite3.
         'PORT': DB_PORT,                # Set to empty string for default. Not used with sqlite3.
+        }
     }
-}
+else:
+    DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        },
+    }
+
 
 # Internationalization
 # https://docs.djangoproject.com/en/1.8/topics/i18n/
@@ -132,10 +140,14 @@ USE_L10N = True
 
 USE_TZ = True
 
+# Initial data
+FIXTURE_DIRS = (os.path.join(REPO_DIR, 'core', 'fixtures'),)
+
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = (os.path.join(REPO_DIR, 'core', 'static'),)
 STATIC_ROOT = os.path.join(WSGI_DIR, 'static')
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
