d0efffaa91036ba389da8ab286f97c3d00ff4708
mUniKeS <munikes@members.fsf.org>
2018-01-21 02:48:07 +0100

    Remove files unnecesary

--- wsgi/learntibetan/core/apps.py
+++
@@ -1,7 +0,0 @@
-from django.apps import AppConfig
-from django.utils.translation import ugettext as _
-
-
-class LearnTibetanConfig(AppConfig):
-    name = _('learntibetan')
-    verbose_name = _('Learn Tibetan')
