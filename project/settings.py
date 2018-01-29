3e0c58071ccaedf0109fedbb8e3670c392cfff24
mUniKeS <munikes@members.fsf.org>
2018-01-30 00:38:18 +0100

    Add directive locale

--- project/settings.py
+++ project/settings.py
@@ -56,6 +56,7 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'whitenoise.middleware.WhiteNoiseMiddleware',
+    'django.middleware.locale.LocaleMiddleware',
 ]
 
 ROOT_URLCONF = 'project.urls'
